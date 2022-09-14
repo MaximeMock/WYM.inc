453acb10bc2dceb4929f38f5253298f3b30d5d41
Mathieu VIALAT <mathieu.vialat@le-campus-numerique.fr>
2022-09-14 14:21:10 +0200

    Connexion BDD + ML + app avec docker-compose

--- web-app/web_app/db_utils.py
+++ web-app/web_app/db_utils.py
@@ -1,13 +1,12 @@
-
 from sqlalchemy_utils import *
 from sqlalchemy.orm import *
 from sqlalchemy import *
 from datetime import datetime 
 #-----
 
+Base = declarative_base()
 
-
-class User(declarative_base()):
+class User(Base):
      __tablename__ = "wym_admin"
 
      _id = Column(Integer, primary_key=True)
@@ -26,47 +25,48 @@ class User(declarative_base()):
 #session.add(userX)
 #session.commit()
 
-class DB(port:str, log:str, password:str, nom_DB):
+class DB:#(port:str, log:str, password:str, nom_DB):
 
 	def __init__(self, port='5432', log='wym_admin', password='admin', nom_DB='wym_admin'):
 		self.port =port
 		self.log =log 
 		self.password =password
 		self.nom_DB =nom_DB
-		self.path = f'postgresql+psycopg2://{log}:{password}@127.0.0.1:{port}/{nom_DB}'
+		self.path = f'postgresql+psycopg2://{log}:{password}@BDD:{port}/{nom_DB}'
 		self.engine = False
 	
- 	def create_connection(self):
- 		self.engine = create_engine(path, echo=True)
- 		return self.engine
+	def create_connection(self):
+		self.engine = create_engine(self.path, echo=True)
+		return self.engine
 
 	def check_bdd(self):
 		try:
 			self.engine
-			return engine
+			return self.engine
 		except:
 			return False
-		
-	def check_tables(self):
-		return inspect(self.connection).has_table(self.nom_DB)
 
-		
+	def check_tables(self):
+		return inspect(self.engine).has_table(self.nom_DB)
+
+
 	def init_bdd(self):
 		if self.engine == False:
-			create_database(path)
-		
-		if self.check_tables(self.engine)==False:
+			create_database(self.path)
+
+		if self.check_tables()==False:
 			Base.metadata.create_all(self.engine)
 		Session = sessionmaker(bind=self.engine)
 		session = Session()
 
-	def recup_user(self, _id, comme, mail, name):
+	def recup_user(self, comme, mail, name):
 		now = datetime.now()
 		current_time = now.strftime("%Y/%m/%d %H:%M:%S")
 		Base.metadata.create_all(self.engine)
 		Session = sessionmaker(bind=self.engine)
 		session = Session()
-		userX= User(_id=_id, date=current_time, name=name, mail=mail, commentaire=comm)
+		user_id = session.query(User._id).count()+1
+		userX= User(_id=user_id, date=current_time, name=name, mail=mail, commentaire=comme)
 		session.add(userX)
 		session.commit()
 		return userX
--- web-app/web_app/model_utils.py
+++ web-app/web_app/model_utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def predict(text):
-    url = 'http://172.17.0.3:5000/model/predict'
+    url = 'http://Model_ML:5000/model/predict'
     text_json = {"text": [text]}
     output_first = requests.post(url, json = text_json)
     output_json = output_first.json()
