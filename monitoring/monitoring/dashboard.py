06c780b8e72cbc8d7c03ffeeec7bc802e2bf575f
Maxime <maxime.mock@le-campus-numerique.fr>
2022-09-15 16:02:04 +0200

    final ?

--- monitoring/monitoring/dashboard.py
+++ monitoring/monitoring/dashboard.py
@@ -2,31 +2,56 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from dbStats_utils import *
+from utils import *
 
 st.title('WYM.inc app')
 
 # Add a selectbox to the sidebar:
 add_selectbox = st.sidebar.selectbox(
     'Quels données voulez vous visualiser?',
-    ('Nombre de mots en entrée', 'Fréquence des mots en entrée', 'Nombre de caractères en entrée', 'Nombre de mots en sortie', 'Fréquence des mots en sortie', 'Nombre de caractères en sortie', 'Différence de mots entre entée et sortie', 'Différence de caractères entre entrée et sortie')
+    ('Etat des données', 'Nombre de mots en entrée', 'Fréquence des mots en entrée', 'Nombre de caractères en entrée', 'Nombre de mots en sortie', 'Fréquence des mots en sortie', 'Nombre de caractères en sortie', 'Différence de mots entre entée et sortie', 'Différence de caractères entre entrée et sortie')
 )
 
-db = DB_stats(port='5431', log='wym_admin', password='admin', nom_DB='wym_admin')
-db.create_connection()
+db = DB_stats(port='5430', log='wym_admin', password='admin', nom_DB='wym_admin')
+engine = db.create_connection()
 session = db.init_bdd()
 
-if (add_selectbox == 'Nombre de mots en entrée'):
-	st.subheader(session.query(Stats.id_stat).count()+1)
+
+# Import data:
+data = engine.execute("SELECT * from statistics").fetchall()
+#Creation dataFrame
+df_data = pd.read_sql("SELECT * from statistics", engine)
+# Afficher le DF : 
+
+
+
+
+#SelectBox:
+if (add_selectbox == 'Etat des données'):
+	st.dataframe(df_data)
+elif (add_selectbox == 'Nombre de mots en entrée'):
+	df2 = df_data.loc[:,['id_stat','nb_mot_E']]
+	plot_scatter(df2, 'nb_mot_E', 'id_stat')
+
+	
 elif (add_selectbox == 'Fréquence des mots en entrée'):
-	st.subheader(add_selectbox)
+	length = len(df_data)-1
+	text = df_data.loc[length, 'text_input']
+	plot_wordcloud(text, max_words=20, list_exception=[])
 elif (add_selectbox == 'Nombre de caractères en entrée'):
-	st.subheader(add_selectbox)
+	df2 = df_data.loc[:,['id_stat','nb_carac_E']]
+	plot_scatter(df2, 'nb_carac_E', 'id_stat')
 elif (add_selectbox == 'Nombre de mots en sortie'):
-	st.subheader(add_selectbox)
+	df2 = df_data.loc[:,['id_stat','nb_mot_S']]
+	plot_scatter(df2, 'nb_mot_S', 'id_stat')
 elif (add_selectbox == 'Fréquence des mots en sortie'):
-	st.subheader(add_selectbox)
+	length = len(df_data)-1
+	text = df_data.loc[length, 'text_output']
+	plot_wordcloud(text, max_words=20, list_exception=[])
+	
 elif (add_selectbox == 'Nombre de caractères en sortie'):
-	st.subheader(add_selectbox)
+	df2 = df_data.loc[:,['id_stat','nb_carac_S']]
+	plot_scatter(df2, 'nb_carac_S', 'id_stat')
 elif (add_selectbox == 'Différence de mots entre entée et sortie'):
 	st.subheader(add_selectbox)
 else:
--- monitoring/monitoring/utils.py
+++ monitoring/monitoring/utils.py
@@ -10,11 +10,11 @@ import plotly.express as px
 
 
 #Wordcloud
- def plot_wordcloud(text:str, max_words:int, list_exception:list()):
+def plot_wordcloud(text:str, max_words:int, list_exception:list()):
 	wordcloud = WordCloud(background_color = 'white', stopwords = list_exception, max_words = max_words).generate(text)
 	fig, ax = plt.subplots(figsize = (12, 8))
-	ax.imshow(wordcloud, interpolation = ‘bilinear’)
-	plt.axis(‘off’)
+	ax.imshow(wordcloud, interpolation = 'bilinear')
+	plt.axis('off')
 	st.pyplot(fig)
 
 def deux_wordcloud(text_1:str, text_2:str, label_1:str, label_2:str, max_words:int, list_exception:list()):
@@ -22,9 +22,9 @@ def deux_wordcloud(text_1:str, text_2:str, label_1:str, label_2:str, max_words:i
 	wordcloud2 = WordCloud(background_color = 'white', stopwords = list_exception, max_words = max_words).generate(text_1)
 	fig1, ax1 = plt.subplots(figsize = (12, 8))
 	fig2, ax2 = plt.subplots(figsize = (12, 8))
-	ax1.imshow(wordcloud1, interpolation = ‘bilinear’)
-	ax2.imshow(wordcloud2, interpolation = ‘bilinear’)
-	plt.axis(‘off’)
+	ax1.imshow(wordcloud1, interpolation = 'bilinear')
+	ax2.imshow(wordcloud2, interpolation = 'bilinear')
+	plt.axis('off')
 	fig = make_subplots(rows=rows, cols=cols, subplot_titles=(label_1, label_2))
 	fig.add_trace(ax1, rows=1, cols=1)
 	fig.add_trace(ax2, rows=2, cols=2)
@@ -42,38 +42,38 @@ def plot_scatter(df:pd.DataFrame(), X_label:str, Y_label:str):
 def deux_subplots_horizontal(title_1:str, title_2:str, x_1, y_1, x_2, y_2):
 	fig = make_subplots(rows=rows, cols=cols, subplot_titles=(title_1, title_2))
 	fig.add_trace(
-    			go.Scatter(
-       			 x=x_1, 
-        			 y=y_1),
-        		row=1, col=1
-    			)
+			go.Scatter(
+			 x=x_1, 
+				 y=y_1),
+			row=1, col=1
+			)
 	fig.add_trace(
-    			go.Scatter(
-       			 x=x_2, 
-        			 y=y_2),
-        		row=1, col=2
-    			)    
-    	st.plotly_chart(fig)	
+			go.Scatter(
+			 x=x_2, 
+				 y=y_2),
+			row=1, col=2
+			)
+	st.plotly_chart(fig)	
     	    			
 def deux_subplots_vertical(title_1:str, title_2:str, x_1, y_1, x_2, y_2):
 	fig = make_subplots(rows=rows, cols=cols, subplot_titles=(title_1, title_2))
 	fig.add_trace(
-    			go.Scatter(
-       			 x=x_1, 
-        			 y=y_1),
-        		row=1, col=1
-    			)
+			go.Scatter(
+			 x=x_1, 
+				 y=y_1),
+			row=1, col=1
+			)
 	fig.add_trace(
-    			go.Scatter(
-       			 x=x_2, 
-        			 y=y_2),
-        		row=2, col=1
-    			)    
-    	st.plotly_chart(fig)
+			go.Scatter(
+			 x=x_2, 
+				 y=y_2),
+			row=2, col=1
+			)    
+	st.plotly_chart(fig)
 
 # Bar plot : 	
-def plot_bar(df:pd.DataFrame(), X_label:str, Y_label:str, color="medal"):
-	fig = px.bar(DataFrame, x=X_label, y=Y_label, color=color)		
+def plot_bar(df:pd.DataFrame(), X_label:str, Y_label:str):
+	fig = px.bar(DataFrame, x=X_label, y=Y_label)		
 	st.header(f"Bar Plot {Y_label} function of {X_label}")
 	st.plotly_chart(fig)
 
